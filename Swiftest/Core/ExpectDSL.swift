fb96bf50479e0bb72b6be31135d835c115da063e
Brian Pratt <brian@8thlight.com>
2014-06-18 00:57:32 -0500

    add line numbers/failures to all expectations

--- Swiftest/Core/ExpectDSL.swift
+++ Swiftest/Core/ExpectDSL.swift
@@ -1,18 +1,22 @@
 func expect<T:Comparable>(subject : T, file:String = __FILE__, line:Int = __LINE__) -> Swiftest.ScalarExpectation<T> {
-  return Swiftest.context.currentExample().expect(subject, file: file, line: line)}
+  return __currentEx().expect(subject, file: file, line: line)}
 
 func expect<T:Comparable>(subject : T[], file:String = __FILE__, line:Int = __LINE__) -> Swiftest.ArrayExpectation<T> {
-  return Swiftest.context.currentExample().expect(subject)
+  return __currentEx().expect(subject, file: file, line: line)
 }
 
 func expect(subject:Bool, file:String = __FILE__, line:Int = __LINE__) -> Swiftest.BoolExpectation {
-  return Swiftest.context.currentExample().expect(subject)
+  return __currentEx().expect(subject, file: file, line: line)
 }
 
 func expect<K:Comparable,V:Comparable>(
   subject : Dictionary<K,V>,
   file:String = __FILE__,
   line:Int = __LINE__
-  ) -> Swiftest.DictionaryExpectation<K, V> {
-    return Swiftest.context.currentExample().expect(subject)
+) -> Swiftest.DictionaryExpectation<K, V> {
+  return __currentEx().expect(subject, file: file, line: line)
+}
+
+func __currentEx() -> Swiftest.Example {
+  return Swiftest.context.currentExample()
 }
--- Swiftest/Core/SpecDSL.swift
+++ Swiftest/Core/SpecDSL.swift
@@ -3,18 +3,22 @@ func describe(target:String, fn:Swiftest.VoidFn, file:String = __FILE__, line:In
 }
 
 func example(subject:String, fn:Swiftest.VoidFn, file:String = __FILE__, line:Int = __LINE__) {
-  Swiftest.context.current().example(subject, fn: fn, file: file, line: line)
+  __current().example(subject, fn: fn, file: file, line: line)
 }
 
 func example(desc: String, file:String = __FILE__, line:Int = __LINE__) {
-  Swiftest.context.current().example(desc, fn: Swiftest.nullFn, file: file, line: line)
+  __current().example(desc, fn: Swiftest.nullFn, file: file, line: line)
 }
 
 func it(desc: String, fn:Swiftest.VoidFn, file:String = __FILE__, line:Int = __LINE__) {
-  Swiftest.context.current().example(desc, fn: fn, file: file, line: line)
+  __current().example(desc, fn: fn, file: file, line: line)
 }
 
 func it(desc: String, file:String = __FILE__, line:Int = __LINE__) {
-  Swiftest.context.current().example(desc, fn: Swiftest.nullFn, file: file, line: line)
+  __current().example(desc, fn: Swiftest.nullFn, file: file, line: line)
+}
+
+func __current() -> Swiftest.Specification {
+  return Swiftest.context.current()
 }
 

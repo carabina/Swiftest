4133b4d1c423f27dddcd7e3f0782f2530cc48f40
Brian Pratt <brian@8thlight.com>
2014-08-26 18:28:51 -0500

    cleanup old stuff

--- src/Swiftest/MobileSpecs/Support/SwiftestController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import Swiftest
-
-class SwiftestController : UIViewController {
-  override func viewDidLoad() {
-    Swiftest.reporter.addListener(ConsoleListener())
-    exit(CInt(Swiftest.run()))
-  }
-}
